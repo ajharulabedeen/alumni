2ec94d005a466ea625ee41bc25fc3f90623520f1
ajharul abedeen <ajharulabedeen@gmail.com>
2019-11-08 08:29:11 +0600

    New Database field added for Basic : addresses and social media links, migration changed.

--- code/backend/database/migrations/2019_10_25_113116_create_profile_basics_table.php
+++ code/backend/database/migrations/2019_10_25_113116_create_profile_basics_table.php
@@ -18,18 +18,22 @@ class CreateProfileBasicsTable extends Migration
             $table->string('user_id',512)->nullable();
             $table->string('dept', 512)->nullable();
             $table->string('batch', 512)->nullable();
-            $table->string('student_id', 512)->nullable();
             $table->string('first_name', 512)->nullable();
+            $table->string('student_id', 512)->nullable();
+            $table->string('passing_year', 512)->nullable();
             $table->string('last_name', 512)->nullable();
             $table->string('birth_date', 512)->nullable();
             $table->string('gender', 512)->nullable();
             $table->string('blood_group', 512)->nullable();
+            $table->string('religion', 512)->nullable();
             $table->string('email', 512)->nullable();
             $table->string('phone', 512)->nullable();
+            $table->string('address_present', 512)->nullable();
+            $table->string('address_permanent', 512)->nullable();
             $table->string('research_interest', 512)->nullable();
             $table->string('skills', 512)->nullable();
+            $table->string('social_media_link', 512)->nullable();
             $table->string('image_address', 512)->nullable();
-            $table->string('religion', 512)->nullable();
             $table->timestamps();
         });
     }
