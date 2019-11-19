4e5c6ecd65af615bf556a9d81c9112b5fd86c824
ajharul abedeen <ajharulabedeen@gmail.com>
2019-11-19 08:08:49 +0600

    documenation added.

--- code/backend/app/profile/Profile_About_Repo_I.php
+++ code/backend/app/profile/Profile_About_Repo_I.php
@@ -6,9 +6,17 @@ use App\profile\ProfileAbout;
 
 interface Profile_About_Repo_I
 {
+    /**
+     *  @param  profileAbout    have to send an new object of "ProfileAbout" model class. Primary key will be set automatcally.
+     *  @return id              after successfull save, id : primary key of the entry, will be returned.
+     *  @uses   to save "About" of an user.
+     */
     public function save(ProfileAbout $profileAbout);
     /**
-     * @return updateStatus boolean, 1 : success, 0 : fail.
+     * @param   profileAboutUpdate  have to send an existing object oif the "About".
+     * @return  updateStatus boolean, 1 : success, 0 : fail.
+     * @uses    to update the "About". If any field null value will not be checked. If any null value send from the font end, the field
+     *          value be set null.
      */
     public function update(ProfileAbout $profileAboutUpdate);
     /**
@@ -25,7 +33,10 @@ interface Profile_About_Repo_I
      */
     public function findOne($id);
     /**
-     * @return about it will return the "About", of the current logged user. no need to send user_id from font.
+     * @return  about it will return the "About", of the current logged user. no need to send user_id from font.
+     * @param   userID  user id of the current logged user have to send. it can be taken automatically from system by an UtilClass.
+     * @uses    "About" of the current logged user will be returned. Generelly, after successfull logging, as initial step,  the current
+     *          logged user about "ngOnint" method.
      */
     public function findAboutByUser($userID);
 }
