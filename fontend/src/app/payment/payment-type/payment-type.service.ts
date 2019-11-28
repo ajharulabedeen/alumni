8355c79db5ff5c8b12b99778a4617118acdb0eee
ajharul abedeen <ajharulabedeen@gmail.com>
2019-11-28 12:08:31 +0600

    payment : Add new Payment done!

--- fontend/src/app/payment/payment-type/payment-type.service.ts
+++ fontend/src/app/payment/payment-type/payment-type.service.ts
@@ -1,15 +1,21 @@
 import { Injectable } from '@angular/core';
 import { PaymentType } from './payment-type.model';
-
+import { AuthService } from '../../auth/auth.service';
+import { HttpClient } from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
 export class PaymentTypeService {
 
-  constructor() { }
+  constructor(private http : HttpClient, private authService : AuthService) { }
 
-  public create(pt : PaymentType){
+  public create(pt: PaymentType) {
     console.log(pt);
+    this.http.post(
+      'http://127.0.0.1:8000/paymentType/create ', pt, this.authService.getHeader()
+    ).subscribe((res: Response) => {
+      console.log(res);
+    });
   }
 
 
--- fontend/src/app/payment/payment.module.ts
+++ fontend/src/app/payment/payment.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { PaymentTypeComponent } from './payment-type/payment-type.component';
 import { LayoutModule } from '../layout/layout.module';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 
 
@@ -11,7 +12,8 @@ import { FormsModule } from '@angular/forms';
   imports: [
     CommonModule,
     LayoutModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ]
 })
 export class PaymentModule { }
