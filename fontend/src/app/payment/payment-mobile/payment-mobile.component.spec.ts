f393bbb5c9c9ba550c9e469fc09e353af05855ed
ajharul abedeen <ajharulabedeen@gmail.com>
2019-12-07 09:44:02 +0600

    payment : payment-mobile - load all payment type!

---
+++ fontend/src/app/payment/payment-mobile/payment-mobile.component.spec.ts
@@ -0,0 +1,25 @@
+// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+// import { PaymentMobileComponent } from './payment-mobile.component';
+
+// describe('PaymentMobileComponent', () => {
+//   let component: PaymentMobileComponent;
+//   let fixture: ComponentFixture<PaymentMobileComponent>;
+
+//   beforeEach(async(() => {
+//     TestBed.configureTestingModule({
+//       declarations: [ PaymentMobileComponent ]
+//     })
+//     .compileComponents();
+//   }));
+
+//   beforeEach(() => {
+//     fixture = TestBed.createComponent(PaymentMobileComponent);
+//     component = fixture.componentInstance;
+//     fixture.detectChanges();
+//   });
+
+//   it('should create', () => {
+//     expect(component).toBeTruthy();
+//   });
+// });
---
+++ fontend/src/app/payment/payment-mobile/payment-mobile.component.ts
@@ -0,0 +1,71 @@
+import { Component, OnInit } from '@angular/core';
+import { PaymentTypeService } from '../payment-type/payment-type.service';
+import { PaymentType } from '../payment-type/payment-type.model';
+
+@Component({
+  selector: 'app-payment-mobile',
+  templateUrl: './payment-mobile.component.html',
+  styleUrls: ['./payment-mobile.component.scss']
+})
+export class PaymentMobileComponent implements OnInit {
+
+  perPage: number;
+  idUpdate: string;
+  updatePaymentType = false;
+  pageNumber: number;
+  total: number;
+  totalPage: number
+
+
+
+
+  ptsArray = new Array();
+
+  constructor(private ptService: PaymentTypeService) { }
+
+  ngOnInit() {
+    document.body.className = 'hold-transition skin-blue sidebar-mini';
+    this.totalPage = (this.total / this.perPage);
+    this.setTotalPaymentType();
+    this.setExistingPayments();
+  }
+
+
+  public setExistingPayments() {
+    this.ptsArray = new Array();
+    this.ptService.getAllPayments(this.perPage, this.pageNumber);
+    this.ptService.pts.subscribe(pt => {
+      for (const key1 in pt) {
+        // console.log(key1);
+        // console.log(pt[key1]['id']);
+        var pt1 = new PaymentType();
+        pt1.$id = pt[key1]["id"];
+        pt1.$name = pt[key1]["name"];
+        pt1.$start_date = pt[key1]["start_date"];
+        pt1.$last_date = pt[key1]["last_date"];
+        pt1.$description = pt[key1]["description"];
+        pt1.$amount = pt[key1]["amount"];
+        this.ptsArray.push(pt1);
+      }
+    });
+    this.setTotalPaymentType();
+  }//method
+
+
+  /**
+   * start of the component all the data will be set.
+   */
+  public setTotalPaymentType() {
+    this.ptService.getTotalCount().subscribe(res => {
+      this.total = res["status"];
+    });
+  }
+
+  public refreshTable() {
+
+  }
+
+
+
+
+}//class
