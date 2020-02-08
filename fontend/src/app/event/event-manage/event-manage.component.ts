64e897ade29daeeac0f93527df07762e5859b939
ajharul abedeen <ajharulabedeen@gmail.com>
2020-02-08 21:01:54 +0600

    event: UI changing- Tab pane added!

--- fontend/src/app/event/event-manage/event-manage.component.ts
+++ fontend/src/app/event/event-manage/event-manage.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-event-manage',
@@ -14,7 +14,8 @@ export class EventManageComponent implements OnInit {
   vanue: string;
   description: string;
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
     window.dispatchEvent(new Event('resize'));
@@ -22,4 +23,23 @@ export class EventManageComponent implements OnInit {
   }
 
 
-}//class
+  // start : for tab in font end.
+  public searchTab(evt, tabName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName('tabcontent');
+    // tabcontent[0].className = 'active';//not working
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = 'none';
+    }
+    tablinks = document.getElementsByClassName('tablinks');
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace('active', '');
+    }
+    document.getElementById(tabName).style.display = 'block';
+    evt.currentTarget.className += ' active';
+  }
+
+  // end : for tab in font end.
+
+
+}// class
