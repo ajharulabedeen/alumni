def140ca00ef16cff319912c8f1e0e75bbf37f70
ajharul abedeen <ajharulabedeen@gmail.com>
2020-02-16 18:00:27 +0600

    event-repo : EventDescriptionNotes class added!

---
+++ code/backend/app/events/EventDescriptionNotes.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: G7
+ * Date: 2/16/2020
+ * Time: 5:57 PM
+ */
+
+namespace App\events;
+
+
+class EventDescriptionNotes
+{
+    public $description;
+    public $notes;
+}
