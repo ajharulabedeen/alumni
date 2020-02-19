08ad68ec05e7dad9f7de7193ceadd18031ae7757
ajharul abedeen <ajharulabedeen@gmail.com>
2020-02-19 17:58:56 +0600

    event-registration : event registration migration and table created!

---
+++ code/backend/database/migrations/2020_02_19_115442_create_event_registrations_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEventRegistrationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('event_registrations', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('user_id',512)->nullable();
+            $table->string('event_id',512)->nullable();
+            $table->date('date',512)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('event_registrations');
+    }
+}
