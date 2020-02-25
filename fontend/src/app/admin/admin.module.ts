d627679c896906385a9bed547e8cccf58568f703
ajharul abedeen <ajharulabedeen@gmail.com>
2020-02-25 22:29:15 +0600

    f-admin: component created!

--- fontend/src/app/admin/admin.module.ts
+++ fontend/src/app/admin/admin.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
+import { AdminComponent } from './admin/admin.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [AdminComponent],
   imports: [
     CommonModule,
     AdminRoutingModule
