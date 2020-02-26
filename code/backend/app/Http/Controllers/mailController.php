abdb12c844f68ecc7e0be8e25ed7629bfbd42efa
ajharul abedeen <ajharulabedeen@gmail.com>
2020-02-26 11:11:44 +0600

    b-mail : template blade file made!

--- code/backend/app/Http/Controllers/mailController.php
+++ code/backend/app/Http/Controllers/mailController.php
@@ -9,9 +9,16 @@ class mailController extends Controller
 {
     public function send()
     {
-        Mail::send(['text' => 'mail'], ['name', 'sharthok'], function ($message) {
-            $message->to('cse1301096@gmail.com', 'To Bitfumes')->subject('Test Mail');
-            $message->from('gub.cse.files@gmail.com', 'Bitfumes');
-        });
+        try {
+//            Mail::send('welcome', ['text' => 'mail'], function ($message) {
+            Mail::send('pass', ['text' => 'mail'], function ($message) {
+                $message->to('cse1301096@gmail.com', 'To Bitfumes')->subject('Test Mail');
+                $message->from('gub.cse.files@gmail.com', 'Bitfumes');
+            });
+            error_log("Mail Send Success!");
+        } catch (\Exception $e) {
+            error_log("Mail Sending Failed!");
+        }
+
     }
 }
---
+++ code/backend/tests/mail/F_Mail_Test.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestUtil;
+
+class F_Mail_Test extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testBasicTest()
+    {
+        $this->assertTrue(true);
+        $this->sendMail();
+
+    }
+
+    public function sendMail()
+    {
+        $response = $this->json(
+            'get',
+            'send',
+            []
+        // ,
+        // [
+        //     "HTTP_AUTHORIZATION" => "bearer" .  $this->getToken("u1@umail.com", "123456")
+        // ]
+        );
+
+        $d = $response->baseResponse->original;
+    }
+
+    // thses method kept to use when feature test with auth will be done.
+
+    public
+    function getToken($mail, $pass)
+    {
+        $response = $this->json(
+            'POST',
+            '/api/login',
+            [
+                'email' => $mail,
+                'password' => $pass
+            ]
+        );
+        $d = $response->baseResponse->original;
+        error_log($d['access_token']);
+    }
+
+    public
+    function Loggin()
+    {
+        $response = $this->json(
+            'POST',
+            '/api/login',
+            [
+                'email' => 'u1@umail.com',
+                'password' => '123456'
+            ]
+        );
+        // dd($response->exception);
+        $d = $response->baseResponse->original;
+        dd($d);
+
+        //working
+        // error_log(" data : ");
+        // error_log(count($d));
+        // error_log(" data : " . $d[0]->user);
+        // $t = json_decode($d);
+        // prettyPrint( $t[0] );
+    }
+
+    public
+    function SignUp()
+    {
+        $response = $this->json(
+            'POST',
+            '/api/signup',
+            [
+                'name' => 'Sally',
+                'email' => 'louiugicar@uttmail.com',
+                'password' => '123456',
+                'password_confirmation' => '123456'
+            ]
+        );
+
+        $response = $this->json(
+            'POST',
+            '/api/signup',
+            [
+                'name' => 'Sally',
+                'email' => 'ouiugicar@uttmail.com',
+                'password' => '123456',
+                'password_confirmation' => '123456'
+            ]
+        );
+        $response = $this->json(
+            'POST',
+            '/api/signup',
+            [
+                'name' => 'Sally',
+                'email' => 'louiugir@uttmail.com',
+                'password' => '123456',
+                'password_confirmation' => '123456'
+            ]
+        );
+        $d = $response->baseResponse->original;
+        dd($d);
+
+        //not working
+        // error_log(" data : ");
+        // error_log(count($d));
+        // error_log(" data : " . $d[0]->user);
+        // dd($d);
+        // $t = json_decode($d);
+        // prettyPrint( $t[0] );
+    }
+}//class
