36de108876e1cbc94b92027d5bd497ac5ac51e3f
ajharul abedeen <ajharulabedeen@gmail.com>
2020-02-26 12:35:57 +0600

    b-mail : mail controller working from new controller!

--- code/backend/app/Mail/MailController.php
+++ code/backend/app/Mail/MailController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
 class MailController extends Controller
 {
@@ -10,5 +11,14 @@ class MailController extends Controller
     {
         error_log(" MAIL :> ");
         error_log(" MAIL : " . __CLASS__);
+        try {
+            Mail::send('pass', ['text' => 'mail'], function ($message) {
+                $message->to('cse1301096@gmail.com', 'To Bitfumes')->subject('Test Mail');
+                $message->from('gub.cse.files@gmail.com', 'Bitfumes');
+            });
+            error_log( "\n Mail Send Success : "  . __CLASS__);
+        } catch (\Throwable $th) {
+            //throw $th;
+        }
     }
 }
