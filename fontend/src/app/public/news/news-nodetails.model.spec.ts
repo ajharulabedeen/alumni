d1298930c80e7242e03b245879fa5e189119c204
ajharul abedeen <ajharulabedeen@gmail.com>
2020-03-02 03:40:23 +0600

    f-news : model class created, no details!

---
+++ fontend/src/app/public/news/news-nodetails.model.spec.ts
@@ -0,0 +1,7 @@
+import { NewsNodetails } from './news-nodetails.model';
+
+describe('NewsNodetails', () => {
+  it('should create an instance', () => {
+    expect(new NewsNodetails()).toBeTruthy();
+  });
+});
---
+++ fontend/src/app/public/news/news-nodetails.model.ts
@@ -0,0 +1,90 @@
+export class NewsNodetails {
+  private id: string;
+  private user_id: string;
+  private title: string;
+  private post_date: string;
+  private images: string;
+
+
+    /**
+     * Getter $id
+     * @return {string}
+     */
+	public get $id(): string {
+		return this.id;
+	}
+
+    /**
+     * Getter $user_id
+     * @return {string}
+     */
+	public get $user_id(): string {
+		return this.user_id;
+	}
+
+    /**
+     * Getter $title
+     * @return {string}
+     */
+	public get $title(): string {
+		return this.title;
+	}
+
+    /**
+     * Getter $post_date
+     * @return {string}
+     */
+	public get $post_date(): string {
+		return this.post_date;
+	}
+
+    /**
+     * Getter $images
+     * @return {string}
+     */
+	public get $images(): string {
+		return this.images;
+	}
+
+    /**
+     * Setter $id
+     * @param {string} value
+     */
+	public set $id(value: string) {
+		this.id = value;
+	}
+
+    /**
+     * Setter $user_id
+     * @param {string} value
+     */
+	public set $user_id(value: string) {
+		this.user_id = value;
+	}
+
+    /**
+     * Setter $title
+     * @param {string} value
+     */
+	public set $title(value: string) {
+		this.title = value;
+	}
+
+    /**
+     * Setter $post_date
+     * @param {string} value
+     */
+	public set $post_date(value: string) {
+		this.post_date = value;
+	}
+
+    /**
+     * Setter $images
+     * @param {string} value
+     */
+	public set $images(value: string) {
+		this.images = value;
+	}
+
+
+}
