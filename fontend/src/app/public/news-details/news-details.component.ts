ce30b5e7b59f3225f082e297245a9f88be93fcbf
ajharul abedeen <ajharulabedeen@gmail.com>
2020-03-05 12:17:32 +0600

    f-news-public : showing news details!

--- fontend/src/app/public/news-details/news-details.component.ts
+++ fontend/src/app/public/news-details/news-details.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
+import {AuthService} from "../../auth/auth.service";
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-news-details',
@@ -9,10 +11,32 @@ import {ActivatedRoute} from "@angular/router";
 export class NewsDetailsComponent implements OnInit {
   private id: string;
 
-  constructor(private activeRoute: ActivatedRoute) { }
+  news_title: string;
+  news_details: string;
+  news_note: string;
+
+  constructor(private activeRoute: ActivatedRoute,
+              private http: HttpClient,
+              private authService: AuthService) {
+  }
 
   ngOnInit() {
     this.id = this.activeRoute.snapshot.params['news_id'];
+    this.getOneNews(this.id);
+  }
+
+  public getOneNews(id: string) {
+    this.http.post(
+      'http://127.0.0.1:8000/news/findOne',
+      {
+        'id': id
+      },
+      this.authService.getHeader()
+    ).subscribe(res => {
+      console.log(res);
+      this.news_title = res['title'];
+      this.news_details = res['description'];
+    });
   }
 
 }// class
--- fontend/src/app/public/news-public/news-public.service.ts
+++ fontend/src/app/public/news-public/news-public.service.ts
@@ -1,10 +1,13 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {AuthService} from "../../auth/auth.service";
+import {HttpClient} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class NewsPublicService {
 
-  constructor() { }
+  constructor() {
+  }
 
 }
