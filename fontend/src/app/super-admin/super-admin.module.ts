31a06d6f2728d613f31c53df811ba75dbfbc1fdd
ajharul abedeen <ajharulabedeen@gmail.com>
2020-03-12 21:10:54 +0600

    f-superadmin : code copied from basic search and administrative!

--- fontend/src/app/super-admin/super-admin.module.ts
+++ fontend/src/app/super-admin/super-admin.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SuperAdminComponent } from './super-admin/super-admin.component';
 import { SuperLogginComponent } from './super-loggin/super-loggin.component';
-
+import {FormsModule} from "@angular/forms";
+import { CKEditorModule } from '@ckeditor/ckeditor5-angular';
 
 
 @NgModule({
   declarations: [SuperAdminComponent, SuperLogginComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    CKEditorModule
   ]
 })
 export class SuperAdminModule { }
